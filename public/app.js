85c8dba05d9aa30bf535f697eef37496c6164c5d
Pandelis Zembashis <info@pandelis.me>
2015-12-16 17:00:38 +0000

    nearly done quiz

--- public/app.js
+++ public/app.js
@@ -24,6 +24,9 @@ app.directive('quiz', function(quizFactory) {
 					scope.question = q.question;
 					scope.options = q.options;
 					scope.answer = q.answer;
+					scope.answer2 = q.answer2;
+					scope.answer3 = q.answer3;
+					scope.answer4 = q.answer4;
 					scope.answerMode = true;
 				} else {
 					scope.quizOver = true;
@@ -36,13 +39,22 @@ app.directive('quiz', function(quizFactory) {
 				var ans = $('input[name=answer]:checked').val();
 
 				if(ans == scope.options[scope.answer]) {
-					scope.score = scope.score + 10;
+					scope.score = scope.score + 20;
 					scope.correctAns = true;
-				} else {
+				} else if (ans == scope.options[scope.answer2]) {
+					scope.score = scope.score + 10;
+					scope.correctAns = false;
+				}else if (ans == scope.options[scope.answer3]) {
+					scope.score = scope.score + 5;
+					scope.correctAns = false;
+				}else if (ans == scope.options[scope.answer4]) {
+					scope.score = scope.score + 1;
 					scope.correctAns = false;
 				}
 
 				scope.answerMode = false;
+
+
 			};
 
 			scope.nextQuestion = function() {
@@ -60,7 +72,10 @@ app.factory('quizFactory', function() {
 		{
 			question: "What are your plans after university?",
 			options: ["Internship", "Graduate Job", "Full Time Job", "Continued Study", "Unsure"],
-			answer: 0
+			answer: 2,
+			answer2: 1,
+			answer3: 0,
+			answer4: 3
 		},
 		{
 			question: "Have you worked part time during university?",
@@ -74,8 +89,8 @@ app.factory('quizFactory', function() {
 		},
 		{
 			question: "Have you got a CV?",
-			options: ["Hell yeah", "Yes", "No", "A C what now?"],
-			answer: 0
+			options: ["Hell yeah", "No", "A C what now?"],
+			answer: 0,
 		},
 		{
 			question: "Does it include a Personal Statement",
@@ -83,19 +98,27 @@ app.factory('quizFactory', function() {
 			answer: 0
 		},
 		{
-			question: "Does your CV include the following?",
-			options: ["About You", "Skills", "Languages"],
+			question: "Have you uncluded an About You section?",
+			options: ["Yes", "No"],
 			answer: 0
 		},
 		{
-			question: "Does it include your Employment/Work Experience?",
-			options: ["Related Skills", "Previous/Current Job", "What were your roles?"],
+			question: "Does it include a Skills section?",
+			options: ["Yes", "No"],
 			answer: 0
 		},
 		{
-			question: "Does it include hobbies and interests?",
-			options:["Volunteering", "Personal Life", "Social Life"],
+			question: "Did you list the languages you can speak?",
+			options: ["Yes", "No"],
 			answer: 0
+		},
+		{
+			question: "How many pages long is your CV?",
+			options: ["1", "2", "3", ">3"],
+			answer: 1,
+			answer2: 0,
+			answer3: 2,
+			answer4: 3
 		}
 	];
 
